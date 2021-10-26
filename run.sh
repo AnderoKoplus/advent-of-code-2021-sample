39532de473b6bf2c5ae4e4ca329e17bebe1c919d
Andero Koplus <andero@opus.ee>
2021-10-26 14:03:43 +0300

    removed whitespace

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 for solution in $(seq -w 1 25)
 do
-  for runCounter in $(seq 1 3)
+  for runCounter in $(seq 1 5)
   do
     filename="day${solution}.jar"
     if [[ -f $filename ]]; then
       echo "> Running solution $solution/25 run $runCounter/3"
       java -jar "${filename}"
       echo "> End running solution $solution/25 run $runCounter/3"
-      sleep 1
+      sleep 5
     fi
     ((ctr++))
   done
